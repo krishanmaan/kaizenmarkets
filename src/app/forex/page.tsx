9aecb45cbd31786ab2444814efcd30b9632d7f52
Krishan Maan <krishankumarmaan400@gmail.com>
2024-09-07 14:12:42 +0530

    h

--- src/app/forex/page.tsx
+++ src/app/forex/page.tsx
@@ -46,19 +46,19 @@ const Page = () => {
           <div className="bg-gradient-to-r p-10 sm:p-20 from-gray-800 to-black w-full text-white text-left rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:shadow-2xl">
             <h1 className="text-2xl sm:text-3xl font-bold pb-6 border-b border-gray-700">Forex Overview</h1>
             <p className="font-light text-sm sm:text-base pt-4">
-            The spot foreign exchange market (also called “Forex” or “FX”) is not traded on exchange but rather accomplished over-the-counter by two counter parties. Initially, Forex was traded “Interbank” since trading was conducted only by banks trading with each other. However, with the development of information systems and technology, financial institutions introduced margin trading on foreign exchange at more competitive terms. At present, the majority of foreign exchange transactions are performed by speculators. Institutional investors, funds, and government institutions contribute to the rest of the market participants. <br /><br />
-            It is estimated that daily Forex Market turnover exceeds 4 trillion U.S. Dollars in volume traded. Forex market today is essentially the largest and most liquid of the financial markets. Forex gives traders an access to the market 24 hours a day, 5 days a week, with prices being quoted to one thousandth of a cent!
+              The spot foreign exchange market (also called “Forex” or “FX”) is not traded on exchange but rather accomplished over-the-counter by two counter parties. Initially, Forex was traded “Interbank” since trading was conducted only by banks trading with each other. However, with the development of information systems and technology, financial institutions introduced margin trading on foreign exchange at more competitive terms. At present, the majority of foreign exchange transactions are performed by speculators. Institutional investors, funds, and government institutions contribute to the rest of the market participants. <br /><br />
+              It is estimated that daily Forex Market turnover exceeds 4 trillion U.S. Dollars in volume traded. Forex market today is essentially the largest and most liquid of the financial markets. Forex gives traders an access to the market 24 hours a day, 5 days a week, with prices being quoted to one thousandth of a cent!
             </p>
           </div>
         </div>
       </div>
 
-      <div className="bg-cover bg-center bg-gray-900 pt-20 pb-20 flex flex-col items-center justify-center">
+      <div className="bg-cover bg-center bg-gray-900 pt-20 pb-20 flex flex-col items-center justify-center -mb-16 ">
         <div className="text-center text-white max-w-screen-xl">
           <p className="text-xl sm:text-2xl mb-8 font-Poppins text-gray-100">Kaizen Markets Trading Advantages</p>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-6 gap-6 mb-12 max-w-screen-xl mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-6  gap-6  max-w-screen-xl mx-auto">
           {[
             { src: ic1, title: 'Fast & Secure Execution' },
             { src: ic8, title: 'Flexible leverage up to 1:400' },
@@ -82,9 +82,9 @@ const Page = () => {
           ))}
         </div>
 
-        <div className="flex flex-col sm:flex-row gap-4 max-w-screen-xl">
+        <div className="flex flex-col sm:flex-row gap-4 max-w-screen-xl pt-12 ">
           <a href="https://office.kaizenmarkets.com/">
-            <button className="bg-yellow-500 text-white py-2 px-6 font-semibold hover:bg-purple-600 transition duration-300">Open Live Account</button>
+            <button className="bg-yellow-500 text-white py-2 px-8 font-semibold hover:bg-purple-600 transition duration-300 ">Open Live Account</button>
           </a>
           <a href="https://office.kaizenmarkets.com/trader/registration">
             <button className="text-white py-2 px-6 font-semibold hover:bg-gray-200 hover:text-gray-800 transition duration-300 border">Open Demo Account</button>
@@ -93,7 +93,10 @@ const Page = () => {
       </div>
 
       <Costs />
-      <StartTradingPage />
+      <div className='mb-10'>
+
+        <StartTradingPage />
+      </div>
       <Footor />
     </div>
   );
