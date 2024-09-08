bcf4b3ba254ced89e3abcc53d4d32f561a906d90
Krishan Maan <krishankumarmaan400@gmail.com>
2024-09-08 21:03:24 +0530

    vb

--- src/app/advantages/page.tsx
+++ src/app/advantages/page.tsx
@@ -6,14 +6,14 @@ import Footor from '../components/Footor';
 import one from '../../../public/img/one.png';
 import phone from '../../../public/img/crop-ios2.png';
 
-interface PageProps {}
+interface PageProps { }
 
 const Page: React.FC<PageProps> = () => {
   return (
-    <div className="w-full text-justify">
+    <div className="w-full ">
       <div className="bg-gradient-custom2 pt-24">
         <Navbar />
-        <div className="p-6 sm:p-8 flex flex-col sm:flex-row items-center justify-between max-w-screen-lg mx-auto">
+        <div className="p-6 sm:p-8 flex flex-col sm:flex-row items-center justify-between max-w-screen-xl mx-auto">
           <div>
             <h1 className="text-gray-900 text-4xl font-bold">Advantages</h1>
             <nav className="text-gray-600 mt-4">
@@ -34,13 +34,15 @@ const Page: React.FC<PageProps> = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col lg:flex-row justify-center items-center text-center lg:text-left space-y-4 lg:space-y-0 lg:space-x-4 p-8">
+      {/* <div className="flex flex-col lg:flex-row justify-center items-center text-center lg:text-left space-y-4 lg:space-y-0 lg:space-x-4 p-8">
         <div className="flex-1">
           <h1 className='font-Poppins text-3xl font-bold'>Download for Android Platform</h1>
-          <p className='font-Inter18 text-sm p-2'>Our strong brand name and recognition combined with our excellent reputation as leaders in the world of Forex trading create a much higher conversion rate, enabling our partners to earn more income swiftly.</p>
+          <p className='font-Inter18 text-sm p-2'>
+            Our strong brand name and recognition combined with our excellent reputation as leaders in the world of Forex trading create a much higher conversion rate, enabling our partners to earn more income swiftly.
+          </p>
           <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
             <div>
-              <ul>
+              <ul className="list-disc ml-5">
                 <li>Weekly Payout</li>
                 <li>No Payout limit</li>
                 <li>Affiliate Tools</li>
@@ -49,9 +51,9 @@ const Page: React.FC<PageProps> = () => {
               </ul>
             </div>
             <div>
-              <ul>
-                <li>Multi–tier upto 2 levels</li>
-                <li>Dedicated Relationship Manage</li>
+              <ul className="list-disc ml-5">
+                <li>Multi–tier up to 2 levels</li>
+                <li>Dedicated Relationship Manager</li>
                 <li>Free Education Video and Tutorials</li>
                 <li>Monthly Contest</li>
                 <li>24/7 support</li>
@@ -60,9 +62,76 @@ const Page: React.FC<PageProps> = () => {
           </div>
         </div>
         <div className="flex-1">
-          <Image src={phone.src} alt="Download for Android Platform" width={300} height={300} className="object-cover" />
+          <Image 
+            src={phone.src} 
+            alt="Download for Android Platform" 
+            width={300} 
+            height={300} 
+            className="object-cover" 
+          />
+        </div>
+      </div> */}
+      <div className="max-w-screen-xl mx-auto flex bg-[#FEFEFE] p-10 flex-col md:flex-row items-center">
+        {/* Text Content */}
+        <div className="md:w-1/2 px-6">
+          <h2 className="text-2xl md:text-3xl font-semibold mb-4">Download for Android Platform</h2>
+          <p className="text-gray-400 mb-8 text-justify">
+            Our strong brand name and recognition combined with our excellent reputation as leaders in the world of Forex trading create a much higher conversion rate, enabling our partners to earn more income swiftly.
+          </p>
+          <h3 className="text-xs mb-4">Increased Trading Choices</h3>
+
+          {/* List Items */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 ">
+            <div className="space-y-2">
+              {[
+                'Weekly Payout',
+                'No Payout limit',
+                'Affiliate Tools',
+                'Marketing Tools',
+                'Hybrid and CPA Commissions plan',
+                
+              ].map((item, index) => (
+                <div className="flex items-start text-xs" key={index}>
+                  <div className="bg-blue-600 rounded-full w-[20px] h-[20px] flex items-center justify-center text-white font-bold text-sm mr-4 flex-shrink-0">
+                    {index + 1  }
+                  </div>
+                  <p className="flex-1">{item}</p>
+                </div>
+              ))}
+            </div>
+
+            <div className="space-y-2">
+              {[
+                'Multi–tier upto 2 levels',
+                'Dedicated Relationship Manage',
+                'Free Education Video and Tutorials',
+                'Monthly Contest',
+                '24/7 support',
+                
+              ].map((item, index) => (
+                <div className="flex items-start text-xs" key={index + 8}>
+                  <div className="bg-blue-600 rounded-full w-[20px] h-[20px] flex items-center justify-center text-white font-bold text-sm mr-4 flex-shrink-0">
+                    {index + 6}
+                  </div>
+                  <p className="flex-1">{item}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* Image Content */}
+        <div className="md:w-1/2 flex justify-center mt-12 md:mt-0">
+          <Image
+            src={phone.src}
+            alt="Trading Platforms"
+            width={400}
+            height={400}
+            className="w-full max-w-sm"
+          />
         </div>
       </div>
+
       <StartTradingPage />
       <Footor />
     </div>
--- src/app/indices/page.tsx
+++ src/app/indices/page.tsx
@@ -24,7 +24,7 @@ const Page: React.FC = () => {
               src={one.src}
               alt="Forex Illustration"
               className="max-w-full h-auto"
-              width={400} 
+              width={400}
               height={240}
               priority
             />
@@ -33,7 +33,7 @@ const Page: React.FC = () => {
       </div>
       <div className="py-8 sm:py-10 px-4 sm:px-6 flex flex-col gap-8 max-w-screen-lg mx-auto items-center justify-center">
         <div className="bg-gradient-to-r p-6 sm:p-10 from-gray-800 to-black w-full text-white text-left rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:shadow-2xl">
-          <h1 className="text-2xl sm:text-3xl font-bold pb-4 border-b border-gray-700">Indices Market</h1>
+          <h1 className="text-2xl sm:text-3xl font-bold pb-4  border-b border-gray-700">Indices Market</h1>
           <p className="font-light text-sm sm:text-base pt-4">
             Kaizen Markets offers one of the largest ranges of stock indices of any online broker. You can choose from a variety of different stock index CFDs, covering markets around the globe, and trade them right alongside your Forex trades. To trade indices, you do not need the services of traditional stock brokerage firms as was the case before the technological advances made in high-speed internet connections. Now you can buy an index or sell an index through online interactive brokers such as Kaizen Markets. Hence, market trading online has become increasingly popular, and retail investors buy indices or sell indices either as investment vehicles or hedging tools to mitigate their portfolio risks. <br /><br />
             The index tracks a basket of stocks that are used as indicators of a specialized segment of a stock exchange such as technology (NASDAQ) or are a general representation of the whole stock market, such as the S&P500.
@@ -44,29 +44,29 @@ const Page: React.FC = () => {
       <div className="container mx-auto py-12 px-4 max-w-screen-xl">
         <div className="grid grid-cols-1 gap-6 text-center">
           <div className="mb-12 pt-16">
-            <h2 className="text-4xl sm:text-5xl font-Poppins">
-              Advantages of Spot Trading with <br /> 
-              <span className="text-yellow-500 font-Poppins font-extrabold">Kaizen Markets</span>
+            <h2 className="text-4xl sm:text-5xl font-Poppins mb-5  ">
+              Advantages of Spot Trading with <br />
             </h2>
+            <span className="text-yellow-500 text-4xl sm:text-5xl  font-Poppins font-extrabold  ">Kaizen Markets</span>
             <p className="mt-4 text-sm sm:text-base font-Inter18">
               Kaizen Markets allows all types of trading strategies including news trading. Kaizen Markets accounts are suitable for all types of traders, from novice traders to real Forex experts! If you are still new to Forex trading, we would suggest you open a Kaizen Markets Demo Account to master your trading skills. Our Demo account features benefits that the live account offers but bears no risk as the funds are virtual. Note that Kaizen Markets Account does not have an expiry date.
             </p>
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 text-white font-Poppins">
-          <div className="p-6 sm:p-8 bg-purple-600 shadow-md rounded-lg flex flex-col justify-center items-center text-center hover:bg-[#F7F6F9] hover:text-black transition duration-200">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6  text-white font-Poppins">
+          <div className="p-6 sm:p-8 bg-[#FE7501] hover:bg-green-600 shadow-md rounded-lg flex flex-col justify-center items-center text-center   transition duration-200">
             <h3 className="text-lg font-semibold mb-2">S&P 500</h3>
             <p className="font-Inter18">The value of the Standard & Poor 500 is based on the share prices of 500 leading US-based companies.</p>
           </div>
-          <div className="p-6 sm:p-8 bg-purple-600 shadow-md rounded-lg flex flex-col justify-center items-center text-center hover:bg-[#F7F6F9] hover:text-black transition duration-200">
+          <div className="p-6 sm:p-8 bg-[#FE7501] shadow-md rounded-lg flex flex-col justify-center items-center text-center hover:bg-green-600  transition duration-200">
             <h3 className="text-lg font-semibold mb-2">NASDAQ 100</h3>
             <p className="font-Inter18">This index represents the value of 100 non-financial NASDAQ-traded companies.</p>
           </div>
-          <div className="p-6 sm:p-8 bg-purple-600 shadow-md rounded-lg flex flex-col justify-center items-center text-center hover:bg-[#F7F6F9] hover:text-black transition duration-200">
+          <div className="p-6 sm:p-8 bg-[#FE7501] shadow-md rounded-lg flex flex-col justify-center items-center text-center hover:bg-green-600  transition duration-200">
             <h3 className="text-lg font-semibold mb-2">FTSE 100</h3>
             <p className="font-Inter18">The FTSE 100 is a collection of 100 of the largest publicly traded UK companies.</p>
           </div>
-          <div className="p-6 sm:p-8 bg-purple-600 shadow-md rounded-lg flex flex-col justify-center items-center text-center hover:bg-[#F7F6F9] hover:text-black transition duration-200">
+          <div className="p-6 sm:p-8 bg-[#FE7501] shadow-md rounded-lg flex flex-col justify-center items-center text-center hover:bg-green-600  transition duration-200">
             <h3 className="text-lg font-semibold mb-2">DJ EURO STOXX 50</h3>
             <p className="font-Inter18">Euro Stoxx 50 collects together 50 of the largest European companies’ share value.</p>
           </div>
